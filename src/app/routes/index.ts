94ed4fda688594ceaf1f81f7efc647c00cea66aa
sabbir1054 <mdsabbir1054@gmail.com>
2025-02-24 22:20:38 +0600

    NIK-10 stater updated

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-
+import { AuthRoutes } from '../module/auth/auth.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
- /*  {
+  {
     path: '/auth',
     route: AuthRoutes,
-  }, */
+  },
 ];
 
-// moduleRoutes.forEach(route => router.use(route.path, route.route));
+moduleRoutes.forEach(route => router.use(route.path, route.route));
 export default router;
