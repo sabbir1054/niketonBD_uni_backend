b9428f1bd9d2cbf82930010e9d1eca52e746bd73
sabbir1054 <mdsabbir1054@gmail.com>
2025-04-16 03:06:38 +0600

    otp genarator correction

--- src/helpers/stringGenrator.ts
+++ src/helpers/stringGenrator.ts
@@ -4,7 +4,7 @@ export const randomString = () => {
 
 export const otpGenerator = () => {
   let otp = '';
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     otp += Math.floor(Math.random() * 10);
   }
   return otp;
